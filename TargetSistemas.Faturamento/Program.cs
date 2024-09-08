45f8f7531b500ab534fcf8ed4f1a61b210f0ee69
Luiz Henrique da Costa <henrique.verde@hotmail.com>
2024-09-08 14:29:33 -0300

    Add project files.

---
+++ TargetSistemas.Faturamento/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Faturamento Anual---------------");
+        Console.WriteLine($"Faturamento mínimo: {Invoicing.getInvoicingMin().ToString("n2")}");
+        Console.WriteLine("================================");
+    }    
+}
+
+public static class Invoicing 
+{
+    private static decimal[] invoicing = { 100m, 200m, 0m, 0m, 500m, 300m, 0m, 450m };
+    private static decimal[] invoicingValue = invoicing.Where(a => a > 0).ToArray();
+
+    public static decimal getInvoicingMin()
+    {
+        return invoicingValue.Min();
+    }
+}
+
